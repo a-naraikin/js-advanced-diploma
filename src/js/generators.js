0efdf5453cc580853ba1c03cebe6b57a41eca14d
a-naraikin <anna.naraikin@yandex.ru>
2021-10-14 00:24:29 +0300

    completed 5 task: Team Generation

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /**
  * Generates random characters
  *
@@ -6,9 +7,21 @@
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
-  // TODO: write logic here
+  const randomIndex = getRandomInt(allowedTypes.length);
+  const RandomItemType = allowedTypes[randomIndex];
+  yield new RandomItemType(maxLevel);
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
-  // TODO: write logic here
+  const membersTeam = [];
+  while (membersTeam.length !== characterCount) {
+    const member = characterGenerator(allowedTypes, maxLevel).next().value;
+    membersTeam.push(member);
+  }
+
+  return membersTeam;
 }
