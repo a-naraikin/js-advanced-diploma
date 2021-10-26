caeb5e32ad3855716bf823b8cd11cca35620879f
a-naraikin <anna.naraikin@yandex.ru>
2021-10-26 17:11:13 +0300

    completed 7 task: Отрисовка команд

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,15 +1,38 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable arrow-body-style */
 import themes from './themes';
+import { generateTeam } from './generators';
+
+import Bowman from './characters/Bowman';
+import Daemon from './characters/Daemon';
+import Magician from './characters/Magician';
+import Swordsman from './characters/Swordsman';
+import Undead from './characters/Undead';
+import Vampire from './characters/Vampire';
+
+import { getPositionedCharacter } from './utils';
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+
+    this.teamPlayer = [Bowman, Daemon, Magician];
+    this.teamСomputer = [Swordsman, Undead, Vampire];
+
+    this.storagePositions = new Set();
+
+    this.positionTeams = getPositionedCharacter([
+      ...generateTeam(this.teamPlayer, 1, 2),
+      ...generateTeam(this.teamСomputer, 1, 2),
+    ], this.storagePositions);
   }
 
   init() {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi(themes.prairie);
+    this.gamePlay.redrawPositions(this.positionTeams);
   }
 
   onCellClick(index) {
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-shadow */
+/* eslint-disable import/named */
+/* eslint-disable import/no-cycle */
+import PositionedCharacter from './PositionedCharacter';
+
 export function calcTileType(index, boardSize) {
   let square = '';
 
@@ -29,4 +34,30 @@ export function calcHealthLevel(health) {
   }
 
   return 'high';
+}
+
+function getRandomPosition(storagePos, initPos) {
+  const boardSize = 8 ** 2;
+  const randomInt = Math.floor(Math.random() * boardSize);
+
+  if (((randomInt - initPos) % 8 === 0
+      || (randomInt - initPos - 1) % 8 === 0)
+      && !(storagePos.has(randomInt))) {
+    storagePos.add(randomInt);
+    return randomInt;
+  }
+
+  return getRandomPosition(storagePos, initPos);
+}
+
+export function getPositionedCharacter(team, storagePos) {
+  return team.map((char) => {
+    let initPosition = 0;
+
+    if (char.type === 'swordsman' || char.type === 'undead' || char.type === 'vampire') {
+      initPosition = 6;
+    }
+
+    return new PositionedCharacter(char, getRandomPosition(storagePos, initPosition));
+  });
 }
